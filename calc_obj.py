705a41df3c2d700752f7b83c81718d52d8ff7fe2
jonathom <jonathanbahlmann@web.de>
2020-12-10 18:39:55 +0100

    first draft

---
+++ calc_obj.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# calculate mining yield
+def calc_mine_yield(mine_list):
+
+    all_yields = []
+    # iterate thorugh list of mining blocks
+    for mine in mine_list:
+
+        # if status is active, add yield to all_yields
+        if(status == "exploitation") {
+            yield = $area * $factor
+            all.yields.append(yield)
+        }
+
+
+    return(np.array(all_yields))
