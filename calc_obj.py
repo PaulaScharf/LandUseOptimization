edac84e4c219ad25c4fc7020083dd25c2814b755
jonathom <jonathanbahlmann@web.de>
2020-12-11 23:09:37 +0100

    running example

--- calc_obj.py
+++ calc_obj.py
@@ -1,25 +1,58 @@
 import numpy as np
+import geopandas as gpd
 
 # calculate mining yield
-def calc_mine_yield(mine_list):
+
+def calc_mine_yield(population_array):
+    """A function to calculate the yields of a population of mining configurations.
+    Input is a population (numpy array) different table configurations"""
 
     all_yields = []
     # iterate thorugh list of mining blocks
-    for mine in mine_list:
+    for candidate in population_array:
+        yields = 0
+
+        # TODO TYPE of candidate: np.array or GeoPandasDataFrame? now implemented for dataframe
+
+        # this is for geopandas frame, select mining = TRUE
+        # TODO creating a column with true/false results into 1/0 in shp.. investigate
+        true_entries = candidate.loc[candidate['mining'] == 1]
+
+        # go through rows
+        for i, entry in true_entries.iterrows():
+
+            # calculate yield
+            area = entry['AREA_HA']
+            # add to yield of candidate
+            yields = yields + area
+
+        all_yields.append(yields)
 
         # if status is active, add yield to all_yields
-        if(status == "exploitation") {
-            yield = $area * $factor
-            all.yields.append(yield)
-        }
-
+        # if(mining == TRUE) {
+        #     yield = area * factor
+        #     all_yields.append(yield)
+        # }
 
     return(np.array(all_yields))
-    
+
+# small testing code, returns area sum
+fp = "./input_data/test_sample/test_sample.shp"
+mines = gpd.read_file(fp)
+print(calc_mine_yield([mines]))
+
+
+
 # calculate biomass of mining area
-def calc_mine_biomass(mine_list):
+def calc_mine_biomass(mine_array):
+    """A function to calculate the biomass lost in a collection of mining configurations."""
 
     biomass_sum = []
 
+    # TODO copy from above
     for mine in mine_list:
-        # mines are unlikely to only have one initial land cover -> how to handle this in a table?
+
+        # only sum up mining = TRUE biomasses
+
+        biomass_sum.append()
+    return(np.array(biomass_sum))
