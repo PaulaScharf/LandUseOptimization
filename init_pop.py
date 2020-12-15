f386c349dd8d1d03dd884f5fcd2cbd8aa0bbe901
joaherreram <herreram.jahm@gmail.com>
2020-12-15 14:08:01 -0500

    changes

--- init_pop.py
+++ init_pop.py
@@ -1,6 +1,7 @@
 import numpy as np
 import geopandas as gpd
 import random
+import fiona
 
 # make initial population, default_dir is unused
 def initialize_spatial(pop_size, default_directory):
@@ -11,13 +12,15 @@ def initialize_spatial(pop_size, default_directory):
     # read the initial dataset (a table with all 'mining' = FALSE)
     table_in = gpd.read_file("./input_data/test_sample/test_sample.shp")
 
+    #table = gpd.GeoDataFrame(table_in)
+    #print(table)
     # length of input table
     length = len(table_in)
     
     # make specified number of initializations
-    for _ in range(pop_size):
+    for i in range(pop_size):
 
-        new_table = table_in
+        new_table = gpd.GeoDataFrame(table_in)
         new_mining = []
 
         # new column has same length
@@ -27,14 +30,21 @@ def initialize_spatial(pop_size, default_directory):
 
         # replace the mining column
         new_table['mining'] = new_mining
-        all_tables.append(new_table)
+
+        table2safe = new_table.to_numpy()
+        
+        
+        all_tables.append(table2safe)
+
+    
+    #all_tables = np.array(all_tables.items())
         
         # TODO why are in the final table all 'mining' values the same? 
         # i.e. why and how is the first table appended to all_tables lost?
-        print(all_tables)
+    # for key in range(all_tables):
+    print(all_tables[1][:,11])
 
-    # print(all_tables)
-    return np.array(all_tables)
 
+    return all_tables
 
 initialize_spatial(2,"path")
