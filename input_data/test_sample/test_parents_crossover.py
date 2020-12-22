869fa769b4f33e41983066726cb3644fb604b284
PaulaScharf <paulascharf@gmx.de>
2020-12-22 10:15:33 +0100

    reworked crossover

--- input_data/test_sample/test_parents_crossover.py
+++ input_data/test_sample/test_parents_crossover.py
@@ -3,7 +3,7 @@ parent1 = np.array([
 [True,'gold',500,300],
 [False,'diamonds',400,600],
 [True,'gold',300,100],
-[True,'diamonds', 200,800],
+[False,'diamonds', 200,800],
 [False,'gold', 100,300]])
 parent2 = np.array([
 [False,'gold',500,300],
@@ -12,4 +12,3 @@ parent2 = np.array([
 [True,'diamonds',200,800],
 [True,'gold',100,300]])
 population = [parent1, parent2]
-print(population[0][:,0])
--- spatial_crossover.py
+++ spatial_crossover.py
@@ -10,18 +10,16 @@ class SpatialOnePointCrossover(Crossover):
 
     def _do(self, problem, X, **kwargs):
 
-        # what is the data structure? Contains X allready the selected 'mates'?
-        n_matings = X.shape[1]
+        #TODO: is this right?
+        n_matings = len(X)
 
         # child land use maps
         child_mining1 = []
         child_mining2 = []
 
         for _ in range(n_matings):
-            # create genome; just the mining column?
-            # works like this or first create variables table1 and table2 to access column?
-            genome_parent1 = X[0][_]['mining']
-            genome_parent2 = X[1][_]['mining']
+            genome_parent1 = X[0]
+            genome_parent2 = X[1]
 
             # define number of cuts
             num_crossover_points = self.n_points
@@ -32,9 +30,9 @@ class SpatialOnePointCrossover(Crossover):
                                                     len(genome_parent2))), num_cuts)
             cut_points.sort()
 
-            # define initial genome of children
-            genome_child1 = list(genome_parent1)
-            genome_child2 = list(genome_parent2)
+            # define initial genome of children (np.array(list(...)) is used to create a copy)
+            genome_child1 = np.array(list(genome_parent1))
+            genome_child2 = np.array(list(genome_parent2))
 
             # get parts of genome from parents to children
             # used 2 as placeholder, because 0 wouldnt work if we use 0 and 1 for mining = true / false
@@ -45,20 +43,22 @@ class SpatialOnePointCrossover(Crossover):
                         j = j + 1
                 # alternating parent 1 and 0
                 if (j % 2) != 0:
-                    genome_child1[i] = 2.
+                    genome_child1[i][0] = '2'
                 # alternating 0 and parent 2
                 if (j % 2) == 0:
-                    genome_child2[i] = 2.
+                    genome_child2[i][0] = '2'
 
-            # dont know if this works with our tables; think we have to add childs as column again..
-            child1_filled = np.where(genome_child1 == 2, genome_parent2, genome_child1)
-            child2_filled = np.where(genome_child2 == 2, genome_parent1, genome_child2)
-            child1 = X[0][_]
-            child2 = X[1][_]
-            child1['mining'] = child1_filled
-            child2['mining'] = child2_filled
-            child_mining1.append(child1)
-            child_mining2.append(child2)
+            child1_filled = np.where(genome_child1 == '2', genome_parent2, genome_child1)
+            child2_filled = np.where(genome_child2 == '2', genome_parent1, genome_child2)
 
+            child_mining1.append(child1_filled)
+            child_mining2.append(child2_filled)
         return np.array([np.array(child_mining1),
                          np.array(child_mining2)])
+
+############# use following lines for testing #############
+#import sys
+#sys.path.insert(0, 'input_data/test_sample')
+#from test_parents_crossover import *
+#test = SpatialOnePointCrossover(2)
+#Sprint(test._do(1,population))
