213d059841ebb96f9a9f0ab00f3be9af7fa6dc7b
jonathom <jonathanbahlmann@web.de>
2021-02-03 17:23:11 +0100

    study1/2 best maps in /figures

--- visualize_in_R.R
+++ visualize_in_R.R
@@ -1,12 +1,12 @@
-setwd("/home/petra/Studium/SPAT_OPT/LandUseOptimization")
+setwd("/home/petra/Studium/spatopt/LandUseOptimization")
 library(sf)
 library(dplyr)
 library(stars)
 
 # load from /results
-yield <- read.csv("./results/result_F1_yield.csv", sep = ",")
-biomass <- read.csv("./results/result_F2_biomass.csv", sep = ",")
-dist <- read.csv("./results/result_F3_dist.csv", sep = ",")
+yield <- read.csv("./results/study1_inverseMut/result_F1_yield.csv", sep = ",")
+biomass <- read.csv("./results/study1_inverseMut/result_F2_biomass.csv", sep = ",")
+dist <- read.csv("./results/study1_inverseMut/result_F3_dist.csv", sep = ",")
 
 # make into sf data frames
 yield$geometry <- as.character(yield$geometry)
@@ -29,7 +29,7 @@ plot(yield["mining"], pal = c("green", "grey"), main="yield")
 plot(biomass["mining"], pal = c("green", "grey"), main="biomass")
 plot(dist["mining"], pal = c("green", "grey"), main="distance")
 
-write_sf(yield, "./results/best_yield.shp")
-write_sf(biomass, "./results/best_bio.shp")
-write_sf(dist, "./results/best_dist.shp")
+write_sf(yield, "./results/study1_inverseMut/best_yield.shp")
+write_sf(biomass, "./results/study1_inverseMut/best_bio.shp")
+write_sf(dist, "./results/study1_inverseMut/best_dist.shp")
 
